58ad21fa8eb84b3417f2110c1b8c390d17ceceb1
Subhadeep <Subhadeep@SUBHADEEP-PC>
2019-12-03 21:04:43 -0700

    Added FDD Web

---
+++ FDDWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FDDWeb.Startup))]
+namespace FDDWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
