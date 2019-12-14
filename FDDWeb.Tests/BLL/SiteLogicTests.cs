2d681065a9fa4de7b3a03370097165a5a71da794
subhadeepdey <subhadeepdey@gmail.com>
2019-12-13 23:08:40 -0700

    Updated Database

---
+++ FDDWeb.Tests/BLL/SiteLogicTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FDDWeb.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FDDWeb.DAO;
+
+namespace FDDWeb.BLL.Tests
+{
+    [TestClass()]
+    public class SiteLogicTests
+    {
+        public ISiteLogic siteLogic { get; set; }
+
+        public ISiteDao siteDao { get; set; }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            siteDao = new SiteDao();
+            siteLogic = new SiteLogic(siteDao);
+        }
+
+        [TestMethod()]
+        public void GetSiteDataTest()
+        {
+            Assert.IsNotNull(siteLogic.GetSiteData());
+        }
+    }
+}
